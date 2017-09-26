ce529c3687cc40caee9d4c11104f10d87b077a7b
shooterRao <rjw0319@163.com>
2017-09-26 15:01:42 +0800

    sixthCommit

---
+++ demo/Generator.js
@@ -0,0 +1,40 @@
+/* 
+generator函数有多种理解角度。从语法上，首先可以把它理解成，Generator函数
+是一个状态加，封装了多个内部状态。
+
+执行generator函数有多种理解角度。从语法上，首先可以把它理解成，
+generator函数除了状态机，还是一个遍历器对象生成函数。
+返回的遍历器对象，可以依次遍历generator函数内部的每一个状态
+
+*/
+
+function* helloWorldGenerator() {
+    yield 'hello';
+    yield 'world';
+    return 'ending';
+  }
+//调用 Generator 函数后，该函数并不执行，返回的也不是函数运行结果，而是一个指向内部状态的指针对象  
+var hw = helloWorldGenerator();
+
+/* 
+必须调用遍历器对象的next方法，使得指针移向下一个状态。
+也就是说，每次调用next方法，内部指针就从函数头部或上一次停下来的地方开始执行，直到遇到下一个yield表达式（或return语句）为止。
+换言之，Generator 函数是分段执行的，yield表达式是暂停执行的标记，而next方法可以恢复执行。
+*/
+console.log(hw.next());//{ value: 'hello', done: false }
+console.log(hw.next());
+console.log(hw.next());//执行到return时,属性done值变为true
+console.log(hw.next());
+
+//next方法的参数表示上一个yield表达式的返回值
+
+function* foo(x) {
+    var y = 2 * (yield (x + 1 ));
+    var z = yield (y / 3);
+    return (x + y + z);
+}
+
+var a = foo(5);
+console.log(a.next());
+console.log(a.next(12));
+console.log(a.next(13));
---
+++ demo/Object.create&__proto__.js
@@ -0,0 +1,67 @@
+//Object.create polyfill代码
+if(!Object.create){
+    Object.create = function(o) {
+        function F(){}
+        F.prototype = o;
+        return new F();
+    }
+}
+//这段代码使用一个一次性函数F，通过改写它的.prototype属性使其指向想要关联的对象,
+//然后再使用new F()来构造一个新对象进行关联
+//对象关联离不开new，new(创建对象,prototype关联,this绑定,返回对象)
+
+var foo = {
+    something:function() {
+        console.log('show');
+    }
+};
+
+var bar = Object.create( foo );
+bar.something();
+console.log(bar.__proto__ === foo);//true
+console.log(Object.getPrototypeOf(bar) === foo);//true
+console.log(bar.__proto__);
+
+//一些demo
+var fn = function(a) {
+    this.a = a;
+}
+
+var proto = { 
+    getKey:function() {
+        console.log(this.a)
+    }
+}
+
+fn.prototype = proto; //prototype可以改写
+var p = new fn(1);
+p.getKey();
+console.log(Object.getPrototypeOf(p));
+console.log(p.__proto__);
+console.log(p.constructor === fn);//false
+console.log(p.constructor);//Object
+console.log(p);//{ a: 1 }
+
+/* 
+
+  这里有个小坑要注意下,
+  p并没有constructor这一属性，不能是说p是被fn所构造的，
+  p.constructor是委托[[prototype]]链上的fn.prototype,
+  不过重置后的fn.prototype(默认的fn.prototype对象有constructor属性)也没有这个属性,
+  然后它会继续委托，直到委托给委托链顶端的Object.prototypr
+  这个对象有constructor属性,指向内置的Object(...)函数
+  
+*/
+
+
+//__proto__的实现大致是这样的
+Object.defineProperty( Object.prototype,'__proto__',{
+    get:function() {
+        return Object.getPrototypeOf( this );
+    },
+    set:function(o) {
+         Object.setPrototypeOf( this, o );//ES6
+         this.prototype = Object.create(o);//ES5
+    }
+})
+
